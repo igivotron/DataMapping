647001bbe910d8bc93ba47e94219a5d2ba3d299a
Igor Grégoire <54171035+igivotron@users.noreply.github.com>
2023-05-06 21:22:09 +0200

    Update metalmap.py

--- metalmap.py
+++ metalmap.py
@@ -5,6 +5,8 @@ class MetalMap:
         self.matrix = np.zeros((3,2))
         self.originCoords = (0, 0)
         self.cellSize = 1
+        self.decal_x = 0
+        self.decal_y = 0
 
     def addColR(self, n):
         self.matrix = np.c_[self.matrix, np.zeros((np.shape(self.matrix)[0],n))]
@@ -13,7 +15,7 @@ class MetalMap:
         self.matrix = np.c_[np.zeros((np.shape(self.matrix)[0],n)),self.matrix]
 
     def addRowU(self, n):
-        self.matrix = np.r_[self.matrix, np.zeros((n, np.shape(self.matrix)[1]))]
+        self.matrix = np.r_[np.zeros((n, np.shape(self.matrix)[1])), self.matrix]
 
     def addRowD(self,n):
         self.matrix = np.r_[self.matrix, np.zeros((n, np.shape(self.matrix)[1]))]
@@ -32,19 +34,25 @@ class MetalMap:
             y = position[1] // self.cellSize +1
 
         if 0 <= x < np.shape(self.matrix)[0] and 0 <= y < np.shape(self.matrix)[1]:
-            self.matrix[x,y] = value
+            self.matrix[x + self.decal_x, y + self.decal_y] = value
 
         else:
             if 0 < x >= np.shape(self.matrix)[0]:
                 self.addRowD(x-np.shape(self.matrix)[0]+1)
             if 0 < y >= np.shape(self.matrix)[1]:
                 self.addColR(y-np.shape(self.matrix)[1]+1)
-            self.matrix[x,y] = value
+            if x < 0:
+                self.decal_x += abs(x)
+                self.originCoords = (self.decal_x, self.decal_y)
+                self.addRowU(abs(x))
+            
+            if y < 0:
+                self.decal_y += abs(y)
+                self.addColL(abs(y))
+            self.matrix[x + self.decal_x, y + self.decal_y] = value
+            self.originCoords = (self.decal_x, self.decal_y)
+
 
 
     def __str__(self):
         return str(self.matrix)
-
-carte = MetalMap()
-carte.addData((3,3),1)
-print(carte)
