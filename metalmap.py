74bfc3b2f1594899148fb8328e35ff4e4cf28f00
Igor Grégoire <54171035+igivotron@users.noreply.github.com>
2023-05-07 11:51:40 +0200

    Update metalmap.py

--- metalmap.py
+++ metalmap.py
@@ -7,6 +7,7 @@ class MetalMap:
         self.cellSize = 1
         self.decal_x = 0
         self.decal_y = 0
+        self.value = 0
 
     def addColR(self, n):
         self.matrix = np.c_[self.matrix, np.zeros((np.shape(self.matrix)[0],n))]
@@ -19,20 +20,8 @@ class MetalMap:
 
     def addRowD(self,n):
         self.matrix = np.r_[self.matrix, np.zeros((n, np.shape(self.matrix)[1]))]
-
-
-
-    def addData(self,position,value):
-        if position[0] % self.cellSize == 0:
-            x = position[0] // self.cellSize
-        else:
-            x = position[0] // self.cellSize +1
-
-        if position[1] % self.cellSize == 0:
-            y = position[1] // self.cellSize
-        else:
-            y = position[1] // self.cellSize +1
-
+    
+    def addToMatrix(self, value, x, y):
         if 0 <= x < np.shape(self.matrix)[0] and 0 <= y < np.shape(self.matrix)[1]:
             self.matrix[x + self.decal_x, y + self.decal_y] = value
 
@@ -51,8 +40,28 @@ class MetalMap:
                 self.addColL(abs(y))
             self.matrix[x + self.decal_x, y + self.decal_y] = value
             self.originCoords = (self.decal_x, self.decal_y)
+        
 
+    def addData(self,position,value):
+        if position[0] % self.cellSize == 0:
+            x = position[0] // self.cellSize
+        else:
+            x = position[0] // self.cellSize +1
 
+        if position[1] % self.cellSize == 0:
+            y = position[1] // self.cellSize
+        else:
+            y = position[1] // self.cellSize +1
+
+        self.value += value
+        self.count += 1
+        if self.count == self.SEND_FREQUENCY:
+            self.addToMatrix(self.value/self.count, x ,y)
+            self.sendMap()
+            self.count = 0
+            self.value = 0
 
     def __str__(self):
         return str(self.matrix)
+
+
