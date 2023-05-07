eedf5ba69084e6353c8cd27ba467024e6517342f
Igor Grégoire <54171035+igivotron@users.noreply.github.com>
2023-05-07 13:33:08 +0200

    1.2.0

--- metalmap.py
+++ metalmap.py
@@ -2,26 +2,29 @@ import numpy as np
 
 class MetalMap:
     def __init__(self):
-        self.matrix = np.zeros((3,2))
+        self.matrix = -np.ones((3,2))
         self.originCoords = (0, 0)
         self.cellSize = 1
         self.decal_x = 0
         self.decal_y = 0
         self.value = 0
+        self.count = 0
+        self.SEND_FREQUENCY = 1
 
     def addColR(self, n):
-        self.matrix = np.c_[self.matrix, np.zeros((np.shape(self.matrix)[0],n))]
+        self.matrix = np.c_[self.matrix, -np.ones((np.shape(self.matrix)[0],n))]
 
     def addColL(self, n):
-        self.matrix = np.c_[np.zeros((np.shape(self.matrix)[0],n)),self.matrix]
+        self.matrix = np.c_[-np.ones((np.shape(self.matrix)[0],n)),self.matrix]
 
     def addRowU(self, n):
-        self.matrix = np.r_[np.zeros((n, np.shape(self.matrix)[1])), self.matrix]
+        self.matrix = np.r_[-np.ones((n, np.shape(self.matrix)[1])), self.matrix]
 
     def addRowD(self,n):
-        self.matrix = np.r_[self.matrix, np.zeros((n, np.shape(self.matrix)[1]))]
+        self.matrix = np.r_[self.matrix, -np.ones((n, np.shape(self.matrix)[1]))]
     
     def addToMatrix(self, value, x, y):
+
         if 0 <= x < np.shape(self.matrix)[0] and 0 <= y < np.shape(self.matrix)[1]:
             self.matrix[x + self.decal_x, y + self.decal_y] = value
 
@@ -43,25 +46,17 @@ class MetalMap:
         
 
     def addData(self,position,value):
-        if position[0] % self.cellSize == 0:
-            x = position[0] // self.cellSize
-        else:
-            x = position[0] // self.cellSize +1
-
-        if position[1] % self.cellSize == 0:
-            y = position[1] // self.cellSize
-        else:
-            y = position[1] // self.cellSize +1
-
+        x = position[0] // self.cellSize 
+        y = position[1] // self.cellSize 
+        
         self.value += value
         self.count += 1
         if self.count == self.SEND_FREQUENCY:
-            self.addToMatrix(self.value/self.count, x ,y)
-            self.sendMap()
+            print(x,y)
+            self.addToMatrix(self.value/self.count, int(x) ,int(y))
+            #self.sendMap()
             self.count = 0
             self.value = 0
 
     def __str__(self):
         return str(self.matrix)
-
-
